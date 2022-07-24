3b7f7bcd59f0457246b18ad4d8daa998f5fa2972
success96 <Lfawas6@gmail.com>
2022-07-23 23:37:22 -0800

    Nodemailer object updated

--- Nmailer.js
+++ Nmailer.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { json } = require("express");
 const nodemailer = require("nodemailer")
-require('dotenv').config();
+require('dotenv').config({path: __dirname+"/.env" });
 
 
 const app = express();
 app.use(json({extended: false}));
 
-const port = 3000;
+const port =3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -18,26 +18,33 @@ app.listen(port, () => {
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    type: 'OAuth2',
-    user: "lfawas6Gmail.com",
-    pass: "BarCelonea96",
-    clientId: "907615288960-tofvtc4u5jifha6og6eio1jk241jop0e.apps.googleusercontent.com",
-    clientSecret: "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",
-    refreshToken: "1//048Hw3ET-JM2oCgYIARAAGAQSNwF-L9IrC15wDZvGB73j_BwENbsk2ZmSXuL_Uj6H1qbc7NVgocN2Ap-h8Ei69N1LGM1XIkiVers"
+    //type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD
   }
 });
 
 let mailOptions = {
-  from: "folaosebikan@stu.ui.edu.ng",
-  to: "lfawas6@gmail.com",
+  from: "lfawas6@gmail.com",
+  to: "folaosebikan830@stu.ui.edu.ng",
   subject: 'Nodemailer Project',
   text: 'Hi from your nodemailer project'
 };
 
-transporter.sendMail(mailOptions, function(err, data) {
-  if (err) {
-    console.log("Error " + err.message);
-  } else {
-    console.log("Email sent successfully");
+const trans = async ()=> {
+  try {
+    const send = await transporter.sendMail(mailOptions);
+    console.log(send)
+  } catch(err){
+
+    console.log(err.message)
   }
-});
+}
+trans()
+// transporter.sendMail(mailOptions, function(err, data) {
+//   if (err) {
+//     console.log("Error " + err.message);
+//   } else {
+//     console.log("Email sent successfully");
+//   }
+// }); 
